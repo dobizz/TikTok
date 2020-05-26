369f44449e9731e89b7d4638ac282a38c428c571
Ronnie Villanueva <ronnie.code@outlook.com>
2020-05-26 18:46:26 +0800

    Update test_api.py
    Test in posix causing build errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,15 +3,24 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api import TikTok
 
 def test_signURL():
-    print('Testing Signatures')
-    tt = TikTok()
-    url = 'https://www.tiktok.com/'
-    signature1 = tt._signURL(url)
-    signature2 = tt._signURL(url)
+    # for Windows
+    if os.name == 'nt':
+        print('Testing Signatures')
+        tt = TikTok()
+        url = 'https://www.tiktok.com/'
+        signature1 = tt._signURL(url)
+        signature2 = tt._signURL(url)
 
-    assert signature1 is not None, 'Signature 1 is Empty'
-    assert signature2 is not None, 'Signature 2 is Empty'
-    assert signature1 != signature2, 'Duplicate signature'
+        assert signature1 is not None, 'Signature 1 is Empty'
+        assert signature2 is not None, 'Signature 2 is Empty'
+        assert signature1 != signature2, 'Duplicate signature'
+
+    # for Linux
+    elif os.name == 'posix':
+        pass
+
+    else:
+        print(f'{os.name} not supported')
 
 if __name__ == '__main__':
     test_signURL()
